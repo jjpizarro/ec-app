cb016f1ef006644c0309848050cfccb0a36ba7cf
julian.pizarro <julian.pizarro@millicom.com>
2022-04-25 15:38:51 -0500

    config app

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -5,13 +5,13 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     APP_ENV: str = 'dev'
-    DATABASE_USERNAME: str = 'demoadmin'
-    DATABASE_PASSWORD: str = '123123'
-    DATABASE_HOST: str = 'fastapi-db.postgres.database.azure.com'
+    DATABASE_USERNAME: str = 'test'
+    DATABASE_PASSWORD: str = 'test'
+    DATABASE_HOST: str = 'localhost'
     DATABASE_NAME: str = 'myecapp'
     TEST_DATABASE_NAME: str = 'myecapp'
     DATABASE_PARAM: str = "sslmode=require"
-    SQLALCHEMY_DATABASE_URI: Optional[str] = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}?{DATABASE_PARAM}"
+    SQLALCHEMY_DATABASE_URI: Optional[str] = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 5
     JWT_SECRET: str = "wrUSueMVxD54evwrhp9qLTwVUNJeAZPX7vVYgd5NNKq8L5tnbaFbDYZTNRG7S5HqJA6W3TEDPEJJP"
